3f7be83f3a3e0927e596c5bc7ca4852a9c17c1c0
vikramisdev <vs423502@gmail.com>
2024-05-21 01:43:31 +0530

    update

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
 var lyricsUrl = 'https://api.lyrics.ovh/v1/';
 
 function findLyrics() {
-    var songname = document.getElementById("song_name").value;
-    var artistname = document.getElementById("artist_name").value;
-   
-    
-    var request = new XMLHttpRequest();
-    
-    var newUrl = lyricsUrl + artistname + "/" + songname;
-    
+  var songname = document.getElementById("song_name").value;
+  var artistname = document.getElementById("artist_name").value;
 
-    request.open('GET', newUrl);
 
-    request.onreadystatechange = function () {
-        if (this.readyState === XMLHttpRequest.DONE) {
-            if (this.status === 200) {
-                var jsonResponse = JSON.parse(this.responseText);
-                
-               document.getElementById("lyrics-text").innerText = "\nSong Name: " + songname + "\n" + "Artist Name: " + artistname + "\n\n" + jsonResponse.lyrics;
-               
-               
-             $("#song_name").val("");
-             $("#artist_name").val("");
-             
-             $("#lyrics-container").attr('open', true);
-             
-             $("#lyrics-container").scroll();
-            }
-            else if(this.status === 404) {
-                $("#notify").stop().animate({
-                    bottom: '20px'
-                }, 300);
-                
-                setTimeout(function () {
-                    $("#notify").stop().animate({
-                        bottom: '-100px'
-                    }, 300);
-                }, 2000);
-            }
-        }
-    };
+  var request = new XMLHttpRequest();
 
-    request.send();
+  var newUrl = lyricsUrl + artistname + "/" + songname;
+
+
+  request.open('GET', newUrl);
+
+  request.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE) {
+      if (this.status === 200) {
+        var jsonResponse = JSON.parse(this.responseText);
+
+        document.getElementById("lyrics-text").innerText = "\nSong Name: " + songname + "\n" + "Artist Name: " + artistname + "\n\n" + jsonResponse.lyrics;
+
+
+        $("#song_name").val("");
+        $("#artist_name").val("");
+
+        $("#lyrics-container").attr('open', true);
+
+        $("#lyrics-container").scroll();
+      }
+      else if (this.status === 404) {
+        showNotification();
+      }
+    }
+  };
+
+  request.send();
 }
+
+function showNotification(text, duration = 2000) {
+  $("#notify p").text(text);
+  
+  $("#notify").stop().animate({
+    bottom: '20px'
+  }, 300);
+
+  setTimeout(function() {
+    $("#notify").stop().animate({
+      bottom: '-100px'
+    }, 300);
+  }, duration);
+}
+
+function copyLyrics() {
+  var copyText = $("#lyrics-text").text();
+
+  navigator.clipboard.writeText(copyText).then(function() {
+    showNotification("Text Copied !");
+  }).catch(function(error) {
+    showNotification("Couldn't copy Text");
+  });
+}
+
